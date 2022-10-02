19f80a3f77b5415022cf885202d5e99e085cf353
Darrel Valdiviezo <Darrel.Valdiviezo@reveraliving.com>
2022-10-02 04:34:08 -0400

    Added printer with input values

--- App.py
+++ App.py
@@ -3,19 +3,22 @@ import numpy as np
 import itertools as iter
 import re
 
-newdf = {}
-#Constant
-DATA = {
-    "FILE": "FF&E.xlsx",
-    "SUITE_PATTERN": re.compile("\d"),
-    "ITEM_PATTERN": re.compile("\w+$")
-}
-
 #import function 
 
 def fetchData(data):
     return pd.read_excel(data, header = 0)
 
+
+
+
+newdf = {}
+#Constant
+DATA = {
+    "FILE": "FF&E.xlsx",
+    "SUITE_PATTERN": re.compile("\d"),
+    "ITEM_PATTERN": re.compile("\w+$"),
+    
+}
 
 mainDf = fetchData(DATA['FILE'])
 
@@ -37,12 +40,26 @@ def get_Item():
 def get_Cost():
     return mainDf['Line Amount']
 
+def get_CostCenter(site):
+    return "CC"+site+"111"
+
+def makeSeries(text):
+   length =  mainDf['Line Amount'].count()
+   return  length * [text]
 
 
 newdf = {
     "Suite Number" : get_SuiteNum(), 
     "Item": get_Item(),
     "Cost": get_Cost(),
+    "ProjectName": makeSeries(input("Project Name?")),
+    "ProjectID": makeSeries(input("ProjectID?")),
+    "Site": makeSeries("ST"+input("Site")),
+    "Company FROM": makeSeries(input("What Company is this coming from?")),
+    "Company TO": makeSeries(input("Which Company is this going too?")),
+    "Spend Category": makeSeries(input("Spend Category?")),
+    "Cost Center":  makeSeries(get_CostCenter(input("Site")))
+
 
 }
 
